b233c5c1897f3c65efcc44c6dbed25e420537cd9
Tanish Rajput <tanishrajput9@gmail.com>
2025-09-01 23:43:54 +0530

    updated chat_utils

--- app/chat_utils.py
+++ app/chat_utils.py
@@ -1,7 +1,13 @@
 from euriai.langchain import create_chat_model
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+API_KEY = os.getenv("EURI_API_KEY")
 
 def get_chat_model(api_key: str):
-    return create_chat_model(api_key=api_key,
+    return create_chat_model(api_key=api_key or API_KEY,
                              model="gpt-4.1-nano",
                              temperature=0.7)
     
