e15df5746af2ebe46a0ee41617d7ee8e83edea05
Tanish Rajput <tanishrajput9@gmail.com>
2025-09-01 23:06:49 +0530

    updated chat_utils.py

--- app/chat_utils.py
+++ app/chat_utils.py
@@ -2,7 +2,7 @@ from euriai.langchain import create_chat_model
 
 def get_chat_model(api_key: str):
     return create_chat_model(api_key=api_key,
-                             model = "gpt-4.1-nano",
+                             model="gpt-4.1-nano",
                              temperature=0.7)
     
     
